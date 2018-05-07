7b9a08141103aed76bedc8d49ed0c757b285636f
palo kolesar <palo.kolesar@gmail.com>
2018-05-07 12:12:02 +0200

    commit

---
+++ Stones.java
@@ -0,0 +1,45 @@
+public class Stones {
+    private static Stones instance;
+    private static long startMillis;
+    private Console console;
+    private BestTimes bestTimes = new BestTimes();
+    private Field field;
+
+
+    protected Stones() {
+        instance = this;
+        startMillis = System.currentTimeMillis();
+        field = Field.Load();
+        console = new Console();
+        console.newGameStarted(field);
+    }
+
+    public static Stones getInstance() {
+        return instance;
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Stones();
+    }
+
+    public int getPlayingSeconds() {
+        int playingTime;
+        playingTime = (int) ((System.currentTimeMillis() - startMillis) * 0.001);
+        return playingTime;
+    }
+
+    public BestTimes getBestTimes() {
+        return bestTimes;
+    }
+
+//    public Field getField(){
+//        return field;
+//    }
+//
+//    public void setField(Field field){
+//        this.field = field;
+//        field.save();
+//    }
+
+
+}
